fe3aad1af9affd2eb32ee008d67fd90da2876601
Luke Zhang <lukezha@amazon.com>
2025-02-04 14:48:16 -0800

    feat: enable otel in dotnet console app.

--- dotnet/dotnet-otel-console/Program.cs
+++ dotnet/dotnet-otel-console/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+//...
+
+var serviceName = "MyServiceName";
+var serviceVersion = "1.0.0";
+
+var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource(serviceName)
+    .ConfigureResource(resource =>
+        resource.AddService(
+          serviceName: serviceName,
+          serviceVersion: serviceVersion))
+    .AddConsoleExporter()
+    .Build();
+
+var meterProvider = Sdk.CreateMeterProviderBuilder()
+    .AddMeter(serviceName)
+    .AddConsoleExporter()
+    .Build();
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddOpenTelemetry(logging =>
+    {
+        logging.AddConsoleExporter();
+    });
+});
+
+//...
 Console.WriteLine("Hello, World!");
+
+
+tracerProvider.Dispose();
+meterProvider.Dispose();
+loggerFactory.Dispose();

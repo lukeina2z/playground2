1ac355871defc8934a22073ab1a65e0d64a72495
Luke Zhang <lukezha@amazon.com>
2025-02-04 09:28:22 +0800

    feat: add roll dice method.

--- dotnet/dotnet-otel-simple/Program.cs
+++ dotnet/dotnet-otel-simple/Program.cs
@@ -1,3 +1,7 @@
+using System.Globalization;
+
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -7,5 +11,28 @@ app.MapGet("/", () =>
     string s2 = " Hello again!!!";
     return s1 + s2;
 });
+
+app.MapGet("/rolldice/{player?}", HandleRollDice);
+
+string HandleRollDice([FromServices]ILogger<Program> logger, string? player)
+{
+    var result = RollDice();
+
+    if (string.IsNullOrEmpty(player))
+    {
+        logger.LogInformation("Anonymous player is rolling the dice: {result}", result);
+    }
+    else
+    {
+        logger.LogInformation("{player} is rolling the dice: {result}", player, result);
+    }
+
+    return result.ToString(CultureInfo.InvariantCulture);
+}
+
+int RollDice()
+{
+    return Random.Shared.Next(1, 7);
+}
 
 app.Run();

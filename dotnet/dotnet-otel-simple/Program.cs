fb7ee89f2c662f2852a4e4caaeaa28575580d682
Luke Zhang <lukezha@amazon.com>
2025-02-05 11:30:10 -0800

    feat: export signals to otel collector.

--- dotnet/dotnet-otel-simple/Program.cs
+++ dotnet/dotnet-otel-simple/Program.cs
@@ -1,4 +1,3 @@
-
 using System.Diagnostics;
 using System.Net.Http;
 using OpenTelemetry.Logs;
@@ -14,14 +13,15 @@ builder.Services.AddSingleton<Instrumentation>();
 const string serviceName = "OTel-AspNet-Svc";
 const string serviceVer = "1.1.0.0";
 
-//builder.Logging.AddOpenTelemetry(options =>
-//    {
-//        options.SetResourceBuilder(
-//                OpenTelemetry.Resources.ResourceBuilder.CreateDefault().AddService(
-//                    serviceName: serviceName,
-//                    serviceVersion: serviceVer))
-//            .AddConsoleExporter();
-//    });
+builder.Logging.AddOpenTelemetry(options =>
+   {
+        options.SetResourceBuilder(
+        OpenTelemetry.Resources.ResourceBuilder.CreateDefault().AddService(
+                serviceName: serviceName,
+                serviceVersion: serviceVer))
+            .AddOtlpExporter()
+            .AddConsoleExporter();
+   });
 
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(serviceName))
@@ -30,10 +30,12 @@ builder.Services.AddOpenTelemetry()
         .SetSampler(new AlwaysOnSampler())
         .AddHttpClientInstrumentation()
         .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter()
+        .AddConsoleExporter())
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter()
         .AddConsoleExporter());
-    //.WithMetrics(metrics => metrics
-    //      .AddAspNetCoreInstrumentation()
-    //      .AddConsoleExporter());
 
 builder.Services.AddControllers();
 
